81d6d1e970fe74c03199e3d48bbe3f8e4f1ac25d
Anil Kumar Tripathi <anil@wellness360.in>
2023-02-21 20:13:38 +0530

    s3 bucket name updates get from env variables

--- service-security/src/main/java/com/io/rpm/service/security/constants/Constant.java
+++ service-security/src/main/java/com/io/rpm/service/security/constants/Constant.java
@@ -1,11 +1,12 @@
 package com.io.rpm.service.security.constants;
 
 public class Constant {
-    public static final String BUCKET_NAME="rpm-dev-new-assets";
+    public static final String BUCKET_NAME=System.getenv("RPM_S3_BUCKET");
     public static final String DOCUMENT="documents";
     public static final String PATIENT_DOCUMENT="patient_documents";
     public static final String PATIENT_INSURANCE_DOCUMENT="patient_documents";
     public static final String PROFILE_PICK="profilepick";
     public static final String INSURANCE_DOCK="InsuranceDocks";
     public static final String DEFAULT_EMAIL="support@kavanant.com";
+    public static final String PATIENT_FOLDER_PREFIX="patient";
 }
